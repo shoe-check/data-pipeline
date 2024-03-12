e824fdd6a93a7f2c2ef190e3c565a1eba6946d87
winhtaikaung <winhtaikaung28@hotmail.com>
2024-03-12 13:39:24 +0000

    Initialized data loader

---
+++ data_loaders/reddit_site_crawler.py
@@ -0,0 +1,38 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    
+    service = Service(executable_path=f"{os.getcwd()}/data-lab/chromedriver-linux64/chromedriver")
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get("https://www.browserstack.com/")
+    element = driver.find_element(By.NAME, "query")
+    
+    driver.quit()
+
+    return ""
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
